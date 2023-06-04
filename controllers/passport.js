624dbef093c16c8547d1905c0e6ba8d9ead1e75c
HuyTrinh <trnhquchuy@yahoo.com.vn>
2023-06-04 10:34:25 +0700

    update

---
+++ controllers/passport.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local');
+
+const bcrypt = require('bcryptjs');
+
+const models = require('../models');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser( async (id, done) => {
+    try {
+        let user = await models.User.findOne({
+            attributes: ['id', 'email', 'firstName', 'lastName', 'mobile', 'isAdmin'],
+            where: {id}
+        });
+        done(null, user);
+    } catch (error){
+        done(error, null);
+    }
+});
+
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, email, password, done) => {
+    if (email){
+        email = email.toLowerCase();
+    }
+    try{
+        if (!req.user){ 
+            let user = await models.User.findOne({where: {email}});
+            if (!user){
+                return done(null, false, req.flash('loginMessage', 'Email does not exits!'));
+            }
+            if (!bcrypt.compareSync(password, user.password)){ 
+                return done(null, false, req.flash('loginMessage', 'Invalid Password'));
+            }
+            return done(null, user);
+        }
+        done(null, req.user);
+    }catch(error) {
+        done(error)
+    }
+}));
+
+passport.use('local-register', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, email, password, done ) => {
+    if (email){
+        email = email.toLowerCase(); 
+    }
+
+    if (req.user){
+        return done(null, req.user); 
+    }
+    try{
+        let user = await models.User.findOne({ where: {email}});
+        if (user){
+            return done(null, false, req.flash('registerMessage', 'Email is already taken!'));
+        }
+
+        user = await models.User.create({
+            email: email, 
+            password: bcrypt.hashSync(password, bcrypt.genSaltSync(8)),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            mobile: req.body.mobile
+        });
+
+        // thong bao dang ky thanh cong
+
+        done(null, false, req.flash('registerMessage', 'You have registerd successfully. Please login!'));
+
+    } catch (error) {
+        done(error);
+    }
+}))
+
+
+module.exports = passport;
---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5001;
+const expressHandleBars = require('express-handlebars');
+const {createStarList} = require('./controllers/handlebarsHelper');
+const {createPagination} = require('express-handlebars-paginate');
+const redisStore = require('connect-redis').default;
+const {createClient} = require('redis');
+const session = require('express-session');
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+})
+
+redisClient.connect().catch(console.error);
+
+const passport = require('./controllers/passport');
+const flash = require('connect-flash');
+//cau hinh public static 
+app.use(express.static(__dirname + '/public'));
+
+
+//cau hinh su dung express handle bar
+app.engine('hbs', expressHandleBars.engine({
+    
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    },
+    helpers: {
+        createStarList,
+        createPagination
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// cau hinh su dung session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: new redisStore({ client: redisClient}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 20 * 60 * 1000 // 20 mins
+    }
+}));
+//cau hinh su dung passport
+app.use(passport.initialize())
+app.use(passport.session());
+
+//cau hinh su dung connect-flash
+app.use(flash());
+
+//midleware khoi tao gio hang
+app.use((req, res, next) => {
+    let Cart = require('./controllers/cart');
+    req.session.cart = new Cart(req.session.cart ? req.session.cart : {});
+    res.locals.quantity = req.session.cart.quantity;
+    res.locals.isLoggedIn = req.isAuthenticated();
+    next();
+})
+
+//cau hinh route
+app.use('/', require('./routes/indexRoute'));
+app.use('/products', require('./routes/productsRouter'));
+app.use('/users', require('./routes/authRouter'));
+app.use('/users', require('./routes/usersRouter'))
+
+//khoi dong web server
+app.listen(port, () => {
+    console.log(`server is running on ${port}`)
+});
